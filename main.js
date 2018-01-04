61d39644f8f339429828d6435b8723e089a4658a
Sela <selaoren.a@gmail.com>
2018-01-04 14:35:01 +0200

    done with logic and cleanups

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const
     // Module to control application life.
     app = electron.app,
     jetpack = require('fs-jetpack'),
+    path = require('path'),
     moment = require('moment'),
     ipc = require('electron').ipcMain,
     dialog = require('electron').dialog,
@@ -33,7 +34,7 @@ function createWindow() {
 
     // and load the index.html of the app.
     mainWindow.loadURL(`file://${__dirname}/index.html`)
-    
+
     mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
@@ -83,26 +84,26 @@ employee{
 }
 */
 
-function parseLines(file){
+function parseLines(file) {
     let lines = file.split("\n");
     let parsedLines = [];
-    for(let line of lines){
-        if(line.trim()){
+    for (let line of lines) {
+        if (line.trim()) {
             // dont include empty lines 
-            parsedLines.push(parseLine(line));  
+            parsedLines.push(parseLine(line));
         }
     }
     return parsedLines;
 }
 
-function parseLine(line){
-    // moment.format("YYYY-MM-DD HH:MM:SS");
+function parseLine(line) {
+    // date format: "YYYY-MM-DD HH:MM:SS"
     let arr = line.trim().split(/[\s,;\t]+/);   // tabs or spaces 
 
-    if(parseInt(arr[0]) == NaN || parseInt(arr[3]) == NaN){
-        throw "something is wrong with the file, did you edit the file?"
+    if (parseInt(arr[0]) == NaN || parseInt(arr[3]) == NaN || arr.length != 7) {
+        dialog.showErrorBox("Something is wrong with the file", "Did you edit the file?\nMake sure it's in the same format as the other lines");
     }
-// console.log(`${arr[1]} ${arr[2]}`);
+
     return {
         id: arr[0],
         date: moment(`${arr[1]} ${arr[2]}`),
@@ -111,74 +112,66 @@ function parseLine(line){
 }
 
 let parsedLines;
-function handleFileSelected(filePath){
+let saveFilePath;
+function handleFileSelected(filePath) {
     let file = jetpack.read(filePath[0])
     parsedLines = parseLines(file);
-    generateMappingByYearMonth()
+    saveFilePath = filePath[0];
 }
 
 ipc.on('generate-file', function (event, data) {
     generateMappingByYearMonth(data.year, data.month);
 })
 
-function generateMappingByYearMonth(year, month){
+function generateMappingByYearMonth(year, month) {
     // shift that starts at end of month and ends at begining of month is added to the new month
     month--;
     const subset = parsedLines.filter(e => e.date.month() == month && e.date.year() == year);
-    console.log("===========================");
-    console.log(subset);
 
-    
-    // TODO handle overlapping between months 
+    if (subset.length == 0) {
+        // todo send error message to renderer
+        dialog.showErrorBox("No entries on that month", "");
+        return;
+    }
+
     /*
     get the day before, remove all the shifts that ended before the new day 
     attach them to the start of the lines 
     */
-    // get previous day of first month and then remove shifts that ended on that day and include the rest in the list
-    
-    // find previous month
-    const prevMonth = (month + 12 - 1) % 12;
-    let prevYear = year;
-    if(prevMonth == 11){
-        prevYear--;
-    }
-    console.log(parsedLines);
-    const lastDayOfMonth = moment(`${prevYear}-${prevMonth}`).daysInMonth();
-    // get all shifts on that day 
-    const shiftsInLastDayOfMonth = parsedLines.filter(e => {
-        const b = e.date.month() == prevMonth && 
-        e.date.year() == prevYear && 
-        e.date.day() == lastDayOfMonth;
-        // console.log(b, e.date.year(), e.date.month(), e.date.day());
-        return b;
-    }
-    );
-    
-    console.log(prevYear, prevMonth, lastDayOfMonth, shiftsInLastDayOfMonth);
-    cleanShiftsFromLastDay(shiftsInLastDayOfMonth);
+    const lineDateCopy = subset[0].date.clone();
+    const lastDayOfMonth = lineDateCopy.subtract(1, 'months').endOf('month');
 
-    calculateTimes(subset);
+    // get all shifts on that day 
+    const shiftsInLastDayOfMonth = parsedLines.filter(e =>
+        e.date.month() == lastDayOfMonth.month() &&
+        e.date.year() == lastDayOfMonth.year() &&
+        e.date.date() == lastDayOfMonth.date()
+    );
+
+    cleanShiftsFromLastDay(shiftsInLastDayOfMonth);
+    const fullSubset = shiftsInLastDayOfMonth.concat(subset);
+    calculateTimes(fullSubset);
 }
 
-function cleanShiftsFromLastDay(lines){
-    const idsInTime =  [...new Set(lines.map(item => item.id))]; 
+function cleanShiftsFromLastDay(lines) {
+    const idsInTime = [...new Set(lines.map(item => item.id))];
     const elementsToDelete = [];
-    
-    for(const id of idsInTime){
+
+    for (const id of idsInTime) {
         let lookingForEndShift = false;
         let shiftStartLine = null;
-        
-        for(const line of lines){
-            if(line.id == id){
-                if(line.start && !lookingForEndShift){
+
+        for (const line of lines) {
+            if (line.id == id) {
+                if (line.start && !lookingForEndShift) {
                     lookingForEndShift = true;
                     shiftStartLine = line;
                 }
-                else if(!line.start && !lookingForEndShift){
+                else if (!line.start && !lookingForEndShift) {
                     // found an end time of shift without a start time 
                     elementsToDelete.push(line);
                 }
-                else if(!line.start && lookingForEndShift){
+                else if (!line.start && lookingForEndShift) {
                     lookingForEndShift = false;
                     elementsToDelete.push(line, shiftStartLine);
                 }
@@ -186,46 +179,42 @@ function cleanShiftsFromLastDay(lines){
         }
     }
 
-    console.log("before", lines);
-    for(let element of elementsToDelete){
-        const index = array.indexOf(element);
-        
+    for (let element of elementsToDelete) {
+        const index = lines.indexOf(element);
         if (index !== -1) {
             lines.splice(index, 1);
         }
     }
-    console.log("after", lines);
-
 }
 
 const ENDBEFORESTART = `shift ended before it started`;
 const WITHOUTEND = `shift started but didn't end`;
 
-function calculateTimes(lines){
-    const idsInTime =  [...new Set(lines.map(item => item.id))]; 
+function calculateTimes(lines) {
+    const idsInTime = [...new Set(lines.map(item => item.id))];
     const idHoursMap = generateIdHoursMap(idsInTime);
     const errorLines = [];
-    
-    console.log(idsInTime,idHoursMap);
 
-    for(const id of idsInTime){
+    console.log(idsInTime, idHoursMap);
+
+    for (const id of idsInTime) {
         let lookingForEndShift = false;
         let shiftStartLine = null;
         let lastLine = null;
 
-        for(const line of lines){
+        for (const line of lines) {
             lastLine = line;
-            if(line.id == id){
-                if(line.start && !lookingForEndShift){
+            if (line.id == id) {
+                if (line.start && !lookingForEndShift) {
                     lookingForEndShift = true;
                     shiftStartLine = line;
                 }
-                else if(!line.start && !lookingForEndShift){
+                else if (!line.start && !lookingForEndShift) {
                     // found an end time of shift without a start time 
-                    console.log(`shift ended before it started line:`, line);
-                    errorLines.push({problem: ENDBEFORESTART, date: line.date.format("YYYY-MM-DD HH:MM:SS"), id: line.id, start: line.start});
+                    console.log(ENDBEFORESTART, line);
+                    errorLines.push(lineToErrorDTO(line, ENDBEFORESTART));
                 }
-                else if(!line.start && lookingForEndShift){
+                else if (!line.start && lookingForEndShift) {
                     lookingForEndShift = false;
                     const diffHours = moment.duration(line.date.diff(shiftStartLine.date)).asHours();
                     fillHoursPerShift(idHoursMap.get(id), diffHours);
@@ -233,18 +222,42 @@ function calculateTimes(lines){
             }
         }
 
-        if(lookingForEndShift){
+        if (lookingForEndShift) {
             // theres a start of shift without an end 
-            console.log(`theres a start of shift without an end:`, shiftStartLine);
-            errorLines.push(Object.assign({problem: WITHOUTEND, }, lastLine));
+            console.log(WITHOUTEND, shiftStartLine);
+            errorLines.push(lineToErrorDTO(lastLine, WITHOUTEND));
         }
     }
 
     console.log(errorLines);
     mainWindow.webContents.send('error-lines', errorLines);
+    saveDataInFile(idHoursMap, lines[lines.length - 1].date.format("YYYY-MM"));
+}
+
+function lineToErrorDTO(line, problem) {
+    return {
+        problem: problem,
+        date: line.date.format("YYYY-MM-DD HH:MM:SS"),
+        id: line.id,
+        start: line.start
+    }
 }
 
 // todo save data as csv file in the same folder as the source with file name year-month
+function saveDataInFile(idHoursMap, dateString) {
+    const header = 'ID, Name, Total Hours, upto 8, 8-10, 10-12, 12+\n'
+
+    const splitStream = [dateString.concat('\n'), header];
+    for (let e of idHoursMap) {
+        splitStream.push(`${e[0]}, name, ${e[1].totalHours}, ${e[1].h8}, ${e[1].h8_10}, ${e[1].h10_12}, ${e[1].h12} \n`)
+    }
+
+    const stream = splitStream.join('');
+    const dirPath = path.dirname(saveFilePath);
+    const newFilePath = dirPath.concat(`\\${dateString} Employee Hours.csv`);
+    console.log(newFilePath);
+    jetpack.write(newFilePath, stream);
+}
 
 function fillHoursPerShift(employeeHourSum, shiftHours) {
     employeeHourSum.totalHours += shiftHours;
@@ -276,13 +289,13 @@ function generateIdHoursMap(idsInTime) {
 }
 
 ipc.on('open-file-dialog', function (event) {
-  dialog.showOpenDialog({
-    properties: ['openFile']
-  }, function (files) {
-    if (files) {
-        event.sender.send('select-file', files)
-        handleFileSelected(files)
-    }
-  })
+    dialog.showOpenDialog({
+        properties: ['openFile']
+    }, function (files) {
+        if (files) {
+            event.sender.send('select-file', files)
+            handleFileSelected(files)
+        }
+    })
 })
 
--- renderer.js
+++ renderer.js
@@ -1,7 +1,7 @@
 $(function () {
 
     const
-        {dialog} = require('electron').remote,
+        { dialog } = require('electron').remote,
         Conf = require('conf'),
         config = new Conf(),
         fs = require('fs'),
@@ -14,162 +14,69 @@ $(function () {
     setup()
 
     function setup() {
-        // // Check if "Wheit" (Light) theme is selected
-        // if ('Bläk' == config.get('theme')) {
-        //     $('head link#styleSheet').attr('href', 'css/example_dark.css');
-        // }
-
-        // $('.header.row.navi').html(loadTemplate('cmdBox', {}));
-
-        // $('footer').prepend('<img src="img/logo.png" height="24px"/> ' + pjson.productName + ' ' + pjson.version + ' - ');
-
         initContent()
-
-        // // Setup buttons
-        // var cmdBox = $('.cmdBoxNavi');
-
-        // cmdBox.find('[data-toggle=config]').on('click', function () {
-        //     showConfig();
-        // });
-
-        // cmdBox.find('[data-toggle=reload]').on('click', function () {
-        //     //reload();
-        //     initContent(loadTemplate('alert', {type:'info', message:'Reload finished.'}));
-        // });
-
-        // cmdBox.find('[data-toggle=theme]').on('click', function () {
-        //     var e = $('head link#styleSheet');
-
-        //     if (e.attr('href').indexOf('dark') > 0) {
-        //         e.attr('href', 'css/example.css');
-        //     } else {
-        //         e.attr('href', 'css/example_dark.css');
-        //     }
-        // });
-
-        // const lines = [{id: 1, date: new Date(), isStart: true,  problem: "asdasf"},
-        //     {id: 1, date: new Date(), isStart: true, problem: "asdasf"},
-        //     {id: 1, date: new Date(), isStart: true,  problem: "asdasf"}];
-        // fillTable(lines)
-    }
-
-    /**
-     * Load a ejs template.
-     *
-     * @param name
-     * @param object
-     *
-     * @returns {String}
-     */
-    function loadTemplate(name, object) {
-        var tpl = fs.readFileSync(__dirname + '/partials/' + name + '.ejs');
-        return ejs.render(tpl.toString(), object);
     }
 
     function initContent(message) {
         $('#header').html('<h2><img src="img/logo.png" height="70px"/> ' + pjson.productName + ' <code>' + pjson.version + '</code></h2>');
-        $('#content').html(loadTemplate('alert', {type:'info', message:'Hey there&hellip;'}));
         initDropDowns();
         if (message) {
             $('#console').html(message);
         }
     }
 
-    /**
-     * Show the configuration.
-     */
-    // function showConfig() {
-    //     $('#header').html('<h3><img src="img/logo.png" height="70px"/> Configuration</h3>');
-    //     $('#content').html(loadTemplate('config', {o:config}));
-    //     $('#console').html('');
-
-    //     $('#btnSaveConfig').on('click', function () {
-    //         saveConfig();
-    //     });
-
-    //     $('#cfgTheme').on('change', function () {
-    //         var e = $('head link#styleSheet');
-
-    //         if ('Bläk' == $(this).val()) {
-    //             e.attr('href', 'css/example_dark.css');
-    //         } else {
-    //             e.attr('href', 'css/example.css');
-    //         }
-    //     });
-    // }
-
-    /**
-     * Save the configuration.
-     */
-    // function saveConfig() {
-    //     var examplePath = $('#cfgExample').val(),
-    //         theme = $('#cfgTheme').val(),
-    //         debug = $('#cfgDebug').is(':checked') ? true : false;
-
-    //     if (false == fs.existsSync(examplePath)) {
-    //         dialog.showErrorBox('Invalid Path', 'The example directory path is invalid');
-    //         return;
-    //     }
-
-    //     config.set('examplePath', examplePath);
-    //     config.set('debug', debug);
-    //     config.set('theme', theme);
-
-    //     initContent(loadTemplate('alert', {type:'info', message:'Config saved.'}));
-    // }
-
     // file select     
     const selectDirBtn = document.getElementById('select-file')
-    
+
     selectDirBtn.addEventListener('click', function (event) {
-      ipc.send('open-file-dialog')
-    })
-    
-    ipc.on('selected-directory', function (event, path) {
-      document.getElementById('selected-file').innerHTML = `You selected: ${path}`
+        ipc.send('open-file-dialog')
     })
 
-    function initDropDowns(){
-        const year = 2017;// TODO change back 
+    ipc.on('selected-directory', function (event, path) {
+        document.getElementById('selected-file').innerHTML = `You selected: ${path}`
+    })
+
+    function initDropDowns() {
+        const year = 2015;// TODO change back 
         const tillYear = 2050;
         var options = "";
-        for(var y=year; y<=tillYear; y++){
-        options += "<option>"+ y +"</option>";
+        for (var y = year; y <= tillYear; y++) {
+            options += "<option>" + y + "</option>";
         }
         document.getElementById("year").innerHTML = options;
 
-        const month = 11;
+        const month = 1;
         const tillMonth = 12;
         options = "";
-        for(let m=month; m<=tillMonth; m++){
-        options += "<option>"+ m +"</option>";
+        for (let m = month; m <= tillMonth; m++) {
+            options += "<option>" + m + "</option>";
         }
         document.getElementById("month").innerHTML = options;
     }
 
     // TODO send data after parsing the file 
-    function fillTable(lines){
+    function fillTable(lines) {
         const table = document.getElementById("error-line-table");
-        
-        for(let l of lines){
+
+        for (let l of lines) {
             const row = table.insertRow(1);
-            const cell0 = row.insertCell(0);            
+            const cell0 = row.insertCell(0);
             const cell1 = row.insertCell(1);
             const cell2 = row.insertCell(2);
             const cell3 = row.insertCell(3);
             const cell4 = row.insertCell(4);
             cell1.innerHTML = l.id;
-            cell2.innerHTML = l.date; 
+            cell2.innerHTML = l.date;
             cell3.innerHTML = l.start ? 1 : 0;
             cell4.innerHTML = l.problem;
 
-            const checkbox = document.createElement("INPUT"); 
-            checkbox.type = "checkbox"; 
+            const checkbox = document.createElement("INPUT");
+            checkbox.type = "checkbox";
             cell0.appendChild(checkbox);
         }
     }
 
-    function emptyTable(){
+    function emptyTable() {
         const table = document.getElementById("error-line-table");
         const tableHeaderRowCount = 1;
         const rowCount = table.rows.length;
@@ -185,7 +92,7 @@ $(function () {
             month: document.getElementById("month").value
         })
     })
-          
+
     ipc.on('error-lines', function (event, data) {
         // clean the table 
         emptyTable();
